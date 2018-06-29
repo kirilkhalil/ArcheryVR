08719a4a5606e7c6b6d652d9ea098ae3815f659d
kirilkhalil <37983740+kirilkhalil@users.noreply.github.com>
2018-06-29 19:03:59 +0300

    Files required for project.
    SteamVR needs to be imported seperately by user in addition to files provided here.

---
+++ Scripts/ArrowManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowManager : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj; //Viite objektiin josta otetaan kiinni
+    public GameObject arrowPrefab; //Viite arrow prefabiin
+    private GameObject currentArrow; //Viite tämän hetkiseen kädessä olevaan nuoleen
+    
+    // Use this for initialization
+	void Awake () {
+        //trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        LiitaNuoli(); //Varmistetaan että on aina nuoli kädessä.
+	}
+
+    public void LiitaNuoli()
+    {
+        if (currentArrow == null)
+        {
+            currentArrow = Instantiate(arrowPrefab); //Tekee kopion nuolesta ja liittää sen 
+            currentArrow.transform.parent = trackedObj.transform; //Liitetään luotu nuoli oikeaan ohjaimeen
+        }
+    }
+}
